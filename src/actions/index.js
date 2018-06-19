c45bb2a8776a727afac9af159d08cdf8cc88a2ae
Gavin Clay <cgavin40@yahoo.com>
2018-06-19 08:29:01 -0600

    edit

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
 
-const ROOT_URL = 'http://reduxblog.herokuapp.com/api'
-const API_KEY = '?key=TUGGERNUTS1234'
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=JUMPJUMP9878';
 
 export function fetchPosts() {
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
